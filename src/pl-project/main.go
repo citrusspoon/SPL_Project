2322cd8078a78105d7f4d996ef286b22a9f5f558
Scott Schreiber <citrusspoon@gmail.com>
2017-04-01 15:44:51 -0400

    Added link to cheat sheet at the top of main.go

--- src/pl-project/main.go
+++ src/pl-project/main.go
@@ -1,11 +1,13 @@
 package main
 
 // https://golang.org/doc/code.html
+// http://www.newthinktank.com/2015/02/go-programming-tutorial/
 
 import "fmt"
+//import "customer.go"
 
 // http://stackoverflow.com/questions/15049903/how-to-use-custom-packages-in-golang
-// import "pl-project/storeLibs" // Not working? according to above it should
+// import "/pl-project/storeLibs" // Not working? according to above it should
 
 func main() {
 	//fmt.Println(customer{0, 5})
