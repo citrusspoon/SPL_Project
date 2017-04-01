956ff36c5000c5b0df17f7df8b6c595954149249
Scott Schreiber <citrusspoon@gmail.com>
2017-04-01 16:14:22 -0400

    Implemented simple createCustomer function
    Also added some global variables, and also figured out how to semi-decently generate random numbers.

--- src/pl-project/main.go
+++ src/pl-project/main.go
@@ -3,7 +3,12 @@ package main
 // https://golang.org/doc/code.html
 // http://www.newthinktank.com/2015/02/go-programming-tutorial/
 
-import "fmt"
+import ( 
+	"fmt"
+	"math/rand"
+	"time"
+
+)
 //import "customer.go"
 
 // http://stackoverflow.com/questions/15049903/how-to-use-custom-packages-in-golang
@@ -14,9 +19,26 @@ type customer struct {
 	serviceDurationSec int
 }
 
+var currentId = 0
+const MINSERVICEDURATION int = 60
+const MAXSERVICEDURATION int = 600
 
 
 func main() {
-	//fmt.Println(customer{0, 5})
-	fmt.Println("Test")
+
+	fmt.Println(createCustomer())
+	fmt.Println(createCustomer())
+	fmt.Println(createCustomer())
+	fmt.Println(createCustomer())
+
+
+}
+
+func createCustomer() customer{
+
+	s1 := rand.NewSource(time.Now().UnixNano() + int64(rand.Intn(1000000)))
+ 	r1 := rand.New(s1)
+
+	currentId++ //for some reason this can't be passed as an argument
+	return customer{currentId, MINSERVICEDURATION + r1.Intn(MAXSERVICEDURATION - MINSERVICEDURATION)}
 }
