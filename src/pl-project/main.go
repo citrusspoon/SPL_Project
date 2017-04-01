21ed0dc2edc014509b3466907751a8924d173cbf
Scott Schreiber <citrusspoon@gmail.com>
2017-04-01 16:31:13 -0400

    Added "register" struct

--- src/pl-project/main.go
+++ src/pl-project/main.go
@@ -19,24 +19,36 @@ type customer struct {
 	serviceDurationSec int
 }
 
+type register struct {
+	id                 int
+	queueSize		   int
+	maxQueueSize	   int
+	currentlyServicing bool
+}
+
 var currentId = 0
 const MINSERVICEDURATION int = 60
 const MAXSERVICEDURATION int = 600
+const NUMOFREGISTERS int = 5
 
 
 func main() {
 
-	fmt.Println(createCustomer())
-	fmt.Println(createCustomer())
-	fmt.Println(createCustomer())
-	fmt.Println(createCustomer())
+	//var registers[NUMOFREGISTERS] register
+
+	fmt.Println("test")
 
 
 }
 
 func createCustomer() customer{
 
-	s1 := rand.NewSource(time.Now().UnixNano() + int64(rand.Intn(1000000)))
+	/*
+		This is necessary to reset the seed that the random function uses. Otherwise the numbers generated would be the same every time 
+		as Go doesn't reset the seed on its own. Just using the time isn't enough because it runs too fast, so I threw in an extra 
+		random number as well. It seems to be somewhat random. 
+	*/
+	s1 := rand.NewSource(time.Now().UnixNano() + int64(rand.Intn(1000000))) 
  	r1 := rand.New(s1)
 
 	currentId++ //for some reason this can't be passed as an argument
